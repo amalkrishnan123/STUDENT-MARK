972cad4d3c48f6bef4f79e13d81f724309a2641d
amalkrishnan123 <amalbaburaj947@gmail.com>
2025-12-29 12:49:33 +0530

    full completed

--- school/views.py
+++ school/views.py
@@ -3,23 +3,42 @@ from django.contrib.auth import authenticate,login,logout
 from .models import Marks,Student
 from .forms import StudentForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import F
 # Create your views here.
 def home_page(request):
-    marks=Marks.objects.all()
-    total=0
-    for i in marks:
-        total=i.math_mark+i.english_mark+i.malayalam_mark
-        i.total=total
-        i.avg=total/3
+    marks = Marks.objects.select_related('student')
+    dict=[]
+
     if request.GET.get('class'):
-        class_name=request.GET.get('class')
-        marks=marks.filter(student__class_name__name=class_name)
-    elif request.GET.get('division'):
-        division=request.GET.get('division')
-        marks=marks.filter(student__division__name=division)
+        marks = marks.filter(student__class_name__name=request.GET.get('class'))
 
-    return render(request,'home.html',{'marks':marks,'total':total})
+    if request.GET.get('division'):
+        marks = marks.filter(student__division__name=request.GET.get('division'))
 
+    for student in marks:
+        total=student.math_mark+student.english_mark+student.malayalam_mark
+        avg=total/3
+        if avg >=75:
+            grade='A'
+        elif avg >=60 and avg < 75:
+            grade='B'
+        elif avg >=40 and avg <60:
+            grade='C'
+        else:
+            grade='Fail'
+            
+        student_dict={
+            'name':student.student.name,
+            'roll_no':student.student.roll_number,
+            'class':student.student.class_name,
+            'division':student.student.division,
+            'total':total,
+            'average':avg,
+            'grade':grade
+        }
+        dict.append(student_dict)
+
+    return render(request, 'home.html', {'marks': dict})
 def admin_login(request):
     if request.POST:
         username=request.POST.get('username')
